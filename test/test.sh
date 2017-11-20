b8a0569c024affd221b39fc397cce4948022be17
Johann Bauer <jb592@uni-rostock.de>
2017-11-21 00:38:48 +0100

    Run tests for Alpine Edge

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
 cd /tmp/electrumx-installer/electrumx-installer
+
+if [ -e "./test/preinstall/$IMAGE" ]; then
+    "./test/preinstall/$IMAGE"
+fi
+
 ./install.sh
 electrumx_server.py
 
