abd181a50185bb45f779dc3878658718d2148e4b
Gui Iribarren <gui@altermundi.net>
2017-11-22 23:40:18 +0100

    fix conditional blocks, group echo and exit together
    otherwise, the 'exit 1' gets executed whenever the test (which ...) returns 'true'
    
    Signed-off-by: Gui Iribarren <gui@altermundi.net>

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,14 +7,14 @@ fi
 if which git > /dev/null 2>&1; then
     git clone https://github.com/bauerj/electrumx-installer ~/.electrumx-installer
 else
-    which wget > /dev/null 2>&1 && which unzip > /dev/null 2>&1 || echo "Please install git or wget and unzip" && exit 1
+    which wget > /dev/null 2>&1 && which unzip > /dev/null 2>&1 || { echo "Please install git or wget and unzip" && exit 1 ; }
     wget https://github.com/bauerj/electrumx-installer/archive/master.zip -O /tmp/electrumx-master.zip
     unzip /tmp/electrumx-master.zip -d ~/.electrumx-installer
     rm /tmp/electrumx-master.zip
 fi
 cd ~/.electrumx-installer/
 if [[ $EUID -ne 0 ]]; then
-    which sudo > /dev/null 2>&1 || echo "You need to run this script as root" && exit 1
+    which sudo > /dev/null 2>&1 || { echo "You need to run this script as root" && exit 1 ; }
     sudo -H ./install.sh "$@"
 else
     ./install.sh "$@"
