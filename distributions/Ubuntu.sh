6eedca76983712e34f26871e2bc9caad701d3c25
Johann Bauer <bauerj@bauerj.eu>
2018-05-22 14:42:36 +0200

    Remove warning for bionic

--- distributions/Ubuntu.sh
+++ distributions/Ubuntu.sh
@@ -1,5 +1,5 @@
-if [ "$VERSION_ID" != "16.04" ]; then
-	_warning "Only the latest LTS version (16.04) is officially supported (but this will probably work)"
+if [ "$VERSION_ID" != "16.04" ] && [ "$VERSION_ID" != "18.04" ]; then
+	_warning "Only the last two LTS versions (16.04 and 18.04) are officially supported (but this will probably work)"
 fi
 
 . distributions/base.sh
@@ -11,9 +11,11 @@ has_rocksdb_binary=1
 
 function install_python36 {
 	$APT update
-	$APT install -y software-properties-common || _error "Could not install package" 5
-	add-apt-repository -y ppa:jonathonf/python-3.6
-	$APT update
+	if [ "$VERSION_ID" == "16.04" ]; then
+	  $APT install -y software-properties-common || _error "Could not install package" 5
+	  add-apt-repository -y ppa:jonathonf/python-3.6
+	  $APT update
+	fi
 	$APT install -y python3.6 python3.6-dev || _error "Could not install package python3.6" 1
 	ln -s $(which python3.6) /usr/local/bin/python3
 }
