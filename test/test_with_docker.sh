690da4c4839c8fb689080140692c98fe273eda87
Johann Bauer <bauerj@bauerj.eu>
2018-07-14 14:15:26 +0200

    Fix tests

--- test/test_with_docker.sh
+++ test/test_with_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "docker run -v $(pwd)/..:/tmp/electrumx-installer $IMAGE /tmp/electrumx-installer/electrumx-installer/test/test.sh"
 docker run -e IMAGE=$IMAGE -v $(pwd)/..:/tmp/electrumx-installer $IMAGE /tmp/electrumx-installer/electrumx-installer/test/test.sh 2>&1 | tee /tmp/$$.log
-if grep -q "ElectrumX server starting" /tmp/$$.log; then
+if grep -q "required envvar" /tmp/$$.log; then
     echo "Success"
     exit 0
 fi
